45b0cf46cf35aa28057231ea696dfba62ef5c70b
Ogunleye Olawale <wealsegun@gmail.com>
2022-11-22 14:32:31 +0100

    changes

--- crown-clothing-new/src/App.js
+++ crown-clothing-new/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <>
-    </>
+    <div className="categories-container">
+      <div className="category-container">
+        <div></div>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
