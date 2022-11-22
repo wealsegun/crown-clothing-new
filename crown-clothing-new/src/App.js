05e148d343c10f71b84d713b3f72ad4f241f837c
Ogunleye Olawale <wealsegun@gmail.com>
2022-11-22 15:54:23 +0100

    changes

--- crown-clothing-new/src/App.js
+++ crown-clothing-new/src/App.js
@@ -1,8 +1,38 @@
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jacket",
+    },
+    {
+      id: 3,
+      title: "Sneaker",
+    },
+    {
+      id: 4,
+      title: "Women",
+    },
+    {
+      id: 5,
+      title: "Mens",
+    },
+  ];
   return (
     <div className="categories-container">
       <div className="category-container">
-        <div></div>
+        {categories.map(({title}) => (
+          <>
+          {/* <img/> */}
+          <div className="category-body-container">
+            <h2> {title}</h2>
+            <p>Shop Now</p>
+          </div>
+          </>
+        ))}
       </div>
     </div>
   );
