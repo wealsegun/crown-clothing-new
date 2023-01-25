49bf0d9c8dbb85f65f123b3b6f710507e66c32dc
Ogunleye Olawale <wealsegun@gmail.com>
2023-01-25 13:58:04 +0100

    firebase, navigation, links and other react dom implementation

--- crown-clothing-new/src/App.js
+++ crown-clothing-new/src/App.js
@@ -1,8 +1,9 @@
 import { Routes, Route } from "react-router-dom";
-
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
 
 // import Directory from "./components/directory/directory.component";
+
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -10,7 +11,8 @@ const Shop = () => {
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />}>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
         <Route path="/shop" element={<Shop />} />
       </Route>
     </Routes>
