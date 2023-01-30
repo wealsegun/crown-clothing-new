ba94683be98b5373e9f3bfe73fb93f15ee7d9adb
Ogunleye Olawale <wealsegun@gmail.com>
2023-01-30 12:19:26 +0100

    changes on the firebase initialization

---
+++ crown-clothing-new/src/utils/firebase/firebase.utils.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDZ0OdY5hcMR-JGDv8FEMHzEtJeIWe6EDc",
+  authDomain: "crwn-clothing-db-1f93e.firebaseapp.com",
+  projectId: "crwn-clothing-db-1f93e",
+  storageBucket: "crwn-clothing-db-1f93e.appspot.com",
+  messagingSenderId: "786811572084",
+  appId: "1:786811572084:web:baca352fe345292e02e621"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
