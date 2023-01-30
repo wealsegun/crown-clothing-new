6c9544fde25085a75efceb69aacfe248676ca032
Ogunleye Olawale <wealsegun@gmail.com>
2023-01-30 12:48:54 +0100

    setting firebase google sign in request

--- crown-clothing-new/src/utils/firebase/firebase.utils.js
+++ crown-clothing-new/src/utils/firebase/firebase.utils.js
@@ -1,5 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,9 +16,16 @@ const firebaseConfig = {
   projectId: "crwn-clothing-db-1f93e",
   storageBucket: "crwn-clothing-db-1f93e.appspot.com",
   messagingSenderId: "786811572084",
-  appId: "1:786811572084:web:baca352fe345292e02e621"
+  appId: "1:786811572084:web:baca352fe345292e02e621",
 };
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
